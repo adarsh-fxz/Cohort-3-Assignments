b4acbfc74ee35ead2ffc79688d91084d85d649bd
Adarsh Gupta <gadarsh318@gmail.com>
2024-08-12 01:34:41 +0545

    Add palindrome solution

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -4,6 +4,17 @@
 */
 
 function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+  let left = 0;
+  let right = cleanedStr.length - 1;
+  while (left < right) {
+    if (cleanedStr[left] !== cleanedStr[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
   return true;
 }
 
