d559a2a37efb606883bea9b8a2b34bbc998c18c6
Adarsh Gupta <gadarsh318@gmail.com>
2024-08-12 01:29:29 +0545

    Add expenditure-analysis solution

--- week-2/week-2-js/easy/expenditure-analysis.js
+++ week-2/week-2-js/easy/expenditure-analysis.js
@@ -4,16 +4,36 @@
   transactions is an array where each
   Transaction - an object like 
         {
-		id: 1,
-		timestamp: 1656076800000,
-		price: 10,
-		category: 'Food',
-		itemName: 'Pizza',
-	}
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  }
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
 function calculateTotalSpentByCategory(transactions) {
+  if (transactions.length > 0) {
+    let categories = {};
+    
+    transactions.forEach(transaction => {
+      if (categories[transaction.category]) {
+        categories[transaction.category] += transaction.price;
+      } else {
+        categories[transaction.category] = transaction.price;
+      }
+    });
+
+    let result = [];
+    for (let category in categories) {
+      result.push({
+        category: category,
+        totalSpent: categories[category]
+      });
+    }
+    return result;
+  }
   return [];
 }
 
