e61b59bed083230b0cf2432be9d40ac9ef2e8b58
Adarsh Gupta <gadarsh318@gmail.com>
2024-08-12 01:35:02 +0545

    Add times solution

--- week-2/week-2-js/medium/times.js
+++ week-2/week-2-js/medium/times.js
@@ -9,5 +9,16 @@ There is no automated test for this one, this is more for you to understand time
 */
 
 function calculateTime(n) {
-    return 0.01;
+    const start = new Date().getTime();
+
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    const end = new Date().getTime();
+
+    return (end - start) / 1000;
 }
+
+console.log(calculateTime(1000000000));
