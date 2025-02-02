e63acfa5d06a7034efcd0ee4d0e15f19cae28db8
Adarsh Gupta <gadarsh318@gmail.com>
2025-02-03 01:10:53 +0545

    Implement user signup and login logic with validation and password hashing

--- week-4/hard/routes/user.js
+++ week-4/hard/routes/user.js
@@ -1,14 +1,76 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const { User } = require("../database");
+const { validateSignupData, hashPassword } = require("../auth");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    try {
+        const validationResult = validateSignupData(req.body);
+        if (!validationResult.success) {
+            return res.status(400).json({
+                message: validationResult.error.errors
+            });
+        }
+        
+        const { username, email, password } = req.body;
+        const hashedPassword = await hashPassword(password);
+        
+        await User.create({
+            username,
+            email,
+            password: hashedPassword
+        });
+        
+        res.status(201).json({
+            message: "User created successfully"
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "User already exists or server error"
+        });
+    }
 });
 
-router.post('/login', (req, res) => {
-     // Implement user login logic
+router.post('/login', async (req, res) => {
+    // Implement user login logic
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({
+            email
+        })
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: "Invalid password"
+            });
+        }
+
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, process.env.JWT_SECRET);
+
+        res.json({
+            token
+        });
+    } catch (error) {
+        return res.status(500).json({
+            message: "Server error"
+        });
+    }
 });
 
 router.get('/todos', userMiddleware, (req, res) => {
