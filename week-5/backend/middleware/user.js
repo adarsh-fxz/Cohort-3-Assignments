97b3c2af98e3927e5b9c7d62594100818e66887b
Adarsh Gupta <gadarsh318@gmail.com>
2025-02-04 02:22:09 +0545

    Implement JWT authentication middleware for user verification

--- week-5/backend/middleware/user.js
+++ week-5/backend/middleware/user.js
@@ -1,1 +1,26 @@
-//  start writing from here
+const jwt = require("jsonwebtoken");
+const SECRET = process.env.SECRET;
+
+const authenticateJwt = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, SECRET, (err, user) => {
+            if (err) {
+                return res.sendStatus(403).json({
+                    message: "Invalid token"
+                })
+            }
+            req.userId = user.userId;
+            next();
+        });
+    } else {
+        res.sendStatus(401).json({
+            message: "Unauthorized: No token provided"
+        });
+    }
+};
+
+module.exports = {
+    authenticateJwt
+};
